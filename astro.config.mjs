4b2b6cf3f3f0cc60cfa4c00c16057a444ad808ba
Mayank <mayank99@users.noreply.github.com>
2022-09-09 14:06:45 -0400

    Fix vite asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		assetsInclude: ['**/*.webc'],
+		build: {
+			assetsInlineLimit: 0,
+		},
+	},
+});
